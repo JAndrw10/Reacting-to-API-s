60c662bd2389ec94dc9c6c9da716ea82e1e57821
JAndrw10 <Joliver1792@gmail.com>
2021-04-04 00:42:54 -0500

    done but wont compile

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,76 @@
 import React, { useState, useEffect } from 'react';
+import Films from './Films'
+import People from './People'
 
 
 const App = () => {
 
-    const [films, setFilms] = useState([]);
-    const [people, setPeople] = useState([]);
+    const [films, setFilms] = useState([])
+    const [people, setPeople] = useState([])
 
-    const[displayFilms, setDisplayFilms] = useState([]);
-    const[displayPeople, setDisplayPeople] = useState([]);
+    const [loadFilms, setLoadFilms] = useState(false)
+    const [loadPeople, setLoadPeople] = useState(false)
 
-    useEffect(() => {
+
+    let handleLoadFilms = () => {
         fetch('https://ghibliapi.herokuapp.com/films')
+        .then(res => res.json()) // takes it out of kson format so we can use
+        .then(res => setFilms(res))
+
+        setLoadPeople(false)
+        setLoadFilms(true)
+    }
+    let handleLoadPeople = () => {
+        fetch('https://ghibliapi.herokuapp.com/people')
         .then(res => res.json())
-        .then(allFilms => setFilms(allFilms))
-}, []);
+        .then(res => setPeople(res))
+
+        setLoadFilms(false)
+        setLoadPeople(true)
+    }
 
 
+    if (loadFilms == false && loadPeople == false) {
+        return (
+            <>
+            <button onClick={()=> handleLoadFilms()}>Load those Films!</button>
+            <button onClick={()=> handleLoadPeople()}>Load those People</button>
 
-useEffect(() => {
-    fetch('https://ghibliapi.herokuapp.com/people')
-    .then(res => res.json())
-    .then(allPeople => setFilms(allPeople))
-}, []);
-
-
-
-
-return(
-    <main className="container">
-        <section className="row justify-content-center mt-5">
-        {films.map(film => (
-
-        ))};
+            Hello
+            </>
+        )
+    } else if (loadFilms == true) {
+        return (
+            <>
+            <button onClick={()=> handleLoadFilms()}>Load those Films!</button>
+            <button onClick={()=> handleLoadPeople()}>Load those People</button>
 
 
+            {films.map(film => (
+           <Films film={film}/>  // setting a prop called film , so we can use it and setting it. if we clicked load those films we get all, 
+            ))}
+        </>
+    )
+  } else if (setLoadFilms == true) {
+    return (
+        <>
+        <button onClick={()=> handleLoadFilms()}>Load those Films!</button>
+        <button onClick={()=> handleLoadPeople()}>Load those People</button>
 
-
-
-        </section>
-
-
-
-
-    </main>
+        {people.map(person => (
+       <People person={person} />  // setting a prop called film , so we can use it and setting it. if we clicked load those films we get all, 
+        ))}
+    </>
 )
+
+  }
+
+}
+
 
 
 
 
 
 export default App;
-}
+
